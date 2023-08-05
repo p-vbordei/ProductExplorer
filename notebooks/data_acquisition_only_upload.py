d185bec83e9ae2ae1cef491adade76b616875ca8
Vlad <bordeivlad@gmail.com>
2023-08-05 11:29:55 +0300

    upload reviews si product data manual in firestore

---
+++ notebooks/data_acquisition_only_upload.py
@@ -0,0 +1,87 @@
+
+# %%
+import aiofiles
+import os
+import json
+from tqdm import tqdm
+
+import time
+import asyncio
+import nest_asyncio
+import aiohttp
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import concurrent.futures
+
+# Firestore details
+cred_path = '/Users/vladbordei/Documents/Development/ProductExplorer/notebooks/productexplorerdata-firebase-adminsdk-ulb3d-465f23dff3.json'
+
+# Initialize Firestore
+cred = credentials.Certificate(cred_path)
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+# %%
+prod_path = '/Users/vladbordei/Documents/Development/ProductExplorer/data/external/product_jsons'
+
+#%%
+async def upload_to_firestore(filename):
+    try:
+        f = await aiofiles.open(os.path.join(prod_path, filename), 'r')
+        data = await f.read()
+        data = json.loads(data)
+        data = data[0]
+        asin = data['asin']
+        doc_ref = db.collection('products').document(asin)
+        doc_ref.set({
+            'details': data,
+        }, merge=True)
+    finally:
+        await f.close()
+
+#%%
+
+async def upload_product_jsons():
+    files = os.listdir(prod_path)
+    tasks = [upload_to_firestore(filename) for filename in tqdm(files)]
+    await asyncio.gather(*tasks)
+
+#%%
+nest_asyncio.apply()
+# to run the function
+loop = asyncio.get_event_loop()
+loop.run_until_complete(upload_product_jsons())
+
+
+# %%
+rev_path = '/Users/vladbordei/Documents/Development/ProductExplorer/data/external/review_jsons'
+db = firestore.client()  # assuming db is defined earlier as your Firestore client
+
+async def upload_reviews_to_firestore(filename):
+    try:
+        f = await aiofiles.open(os.path.join(rev_path, filename), 'r')
+        data = await f.read()
+        data = json.loads(data)
+        data = data[0]
+        asin = data['asin']['original']
+        doc_ref = db.collection('products').document(asin)
+        doc_ref.set({
+            'reviews': data,
+        }, merge=True)
+    finally:
+        await f.close()
+
+async def upload_review_jsons():
+    files = os.listdir(rev_path)
+    tasks = [upload_reviews_to_firestore(filename) for filename in tqdm(files)]
+    await asyncio.gather(*tasks)
+
+nest_asyncio.apply()
+# to run the function
+loop = asyncio.get_event_loop()
+loop.run_until_complete(upload_review_jsons())
+
+
+# %%
--- notebooks/rev_2a.py
+++ notebooks/rev_2a.py
@@ -29,7 +29,6 @@ def read_data(folder_path):
             file_path = os.path.join(folder_path, file_name)
             df = pd.read_csv(file_path)
             reviews = pd.concat([reviews, df])
-    
     return reviews
 
 
@@ -62,3 +61,84 @@ except:
 # save_path = './data/interim/reviews.csv'
 save_path = '/Users/vladbordei/Documents/Development/ProductExplorer/data/interim/reviews.csv'
 reviews.to_csv(save_path, index=False)
+
+# %%
+# https://firebase.google.com/docs/firestore/quickstart?authuser=0&_gl=1*2w73qq*_ga*OTM3NzQyMjU5LjE2OTAzMDU0MTU.*_ga_CW55HF8NVT*MTY5MDQ2MDMyNC4yLjEuMTY5MDQ2MTA3MC4wLjAuMA..#python
+# https://www.youtube.com/watch?v=b4W3YQdViTI
+# https://www.youtube.com/watch?v=N0j6Fe2vAK4
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account.
+cred = credentials.Certificate('/Users/vladbordei/Documents/Development/ProductExplorer/notebooks/productexplorerdata-firebase-adminsdk-ulb3d-465f23dff3.json')
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+# %%
+
+
+
+
+import time
+import pandas as pd
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Your ASINs
+asin_list = ['B08X2324ZL', 'B09MQ689XL']
+
+# Firestore details
+cred_path = '/Users/vladbordei/Documents/Development/ProductExplorer/notebooks/productexplorerdata-firebase-adminsdk-ulb3d-465f23dff3.json'
+
+# Initialize Firestore
+cred = credentials.Certificate(cred_path)
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+# Loading details from CSV
+def load_details_from_csv(asin):
+    # Assuming your dataframe is named df and the ASIN column is named 'asin'
+    df = pd.read_csv('details.csv')
+    details = df[df['asin'] == asin].to_dict('records')[0] # We assume that there is only one detail row per ASIN
+    return details
+
+# Loading reviews from CSV
+def load_reviews_from_csv(asin):
+    # Assuming your dataframe is named df and the ASIN column is named 'asin'
+    df = pd.read_csv('reviews.csv')
+    reviews = df[df['asin'] == asin].to_dict('records')
+    return reviews
+
+def update_firestore(asin, details, reviews):
+    if details is None or reviews is None:
+        print(f"Skipping Firestore update for {asin} due to missing data.")
+        return
+    start = time.time()
+    doc_ref = db.collection('products').document(asin)
+    doc_ref.set({
+        'details': details,
+        'reviews': reviews
+    }, merge=True)
+    print(f"Updating Firestore for {asin} took {time.time() - start} seconds.")
+
+def process_asin(asin):
+    details = load_details_from_csv(asin)
+    if details is None:
+        print(f"Skipping {asin} due to failed details fetch.")
+        return
+    reviews = load_reviews_from_csv(asin)
+    if reviews is None:
+        print(f"Skipping {asin} due to failed reviews fetch.")
+        return
+    update_firestore(asin, details, reviews)
+
+def main():
+    for asin in asin_list:
+        process_asin(asin)
+
+main()
