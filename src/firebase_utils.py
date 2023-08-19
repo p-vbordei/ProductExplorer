2ec1f6118982cda25ba3589f1a6e681c1f87c93c
Vlad <bordeivlad@gmail.com>
2023-08-19 21:46:42 +0300

    added a new secrets provider

--- src/firebase_utils.py
+++ src/firebase_utils.py
@@ -23,7 +23,7 @@ except ImportError:
 
 def get_secret(secret_name):
     client = secretmanager.SecretManagerServiceClient()
-    project_id = "productexplorer"
+    project_id = "productexplorerdata"
     secret_version_name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
     response = client.access_secret_version(request={"name": secret_version_name})
     return response.payload.data.decode('UTF-8')
