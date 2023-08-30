f58cd32d1188c318f2c33c4aead28370f3c69069
Vlad <bordeivlad@gmail.com>
2023-08-30 15:20:27 +0300

    more error handling

--- src/reviews_processing.py
+++ src/reviews_processing.py
@@ -2,6 +2,8 @@
 # reviews_processing.py
 # %%
 import asyncio
+from tqdm import tqdm
+import time
 import logging
 
 try:
@@ -19,139 +21,156 @@ except ImportError:
 
 ####################################### PROCESS REVIEWS WITH GPT #######################################
 
+
 def process_reviews_with_gpt(reviewsList, db):
-    reviewFunctions = [
-        {
-            "name": "reviewDataFunction",
-            "description": "product description",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "reviewSummary": {
-                        "type": "string",
-                        "description": "A brief summary of the review. Eg: Good product overall, but improvements can be made in battery life and noise levels."
+    try:
+        # Start the timer
+        start_time = time.time()
+
+        # Define review functions
+        reviewFunctions = [
+            {
+                "name": "reviewDataFunction",
+                "description": "product description",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "reviewSummary": {
+                            "type": "string",
+                            "description": "A brief summary of the review. Eg: Good product overall, but improvements can be made in battery life and noise levels."
+                        },
+                        "buyerMotivation": {
+                            "type": "string",
+                            "description": "Reasons why the buyer purchased the product. Eg: to replace an old product, to try out a new product, to give as a gift"
+                        },
+                        "customerExpectations": {
+                            "type": "string",
+                            "description": "Expectations the customer had before purchasing the product. Eg: to be able to use the product for a long time, to be able to use the product in a variety of situations, to be able to use the product for a specific purpose"
+                        },
+                        "howTheProductIsUsed": {
+                            "type": "string",
+                            "description": "Information about what the product is used for or about how the product is used. Eg: doodling, practicing letters/shapes, playing games"
+                        },
+                        "whereTheProductIsUsed": {
+                            "type": "string",
+                            "description": "Suggested locations or situations where the product can be used. Eg: car, restaurant, garden, public parks"
+                        },
+                        "userDescription": {
+                            "type": "string",
+                            "description": "Description of the user for the product. Eg: children, preschoolers, basketball players, mothers, office workers"
+                        },
+                        "packaging": {
+                            "type": "string",
+                            "description": "Description of the product's packaging. Eg: sturdy recyclable box, wrapped in plastic, great for gifting"
+                        },
+                        "season": {
+                            "type": "string",
+                            "description": "Eg: fall and winter"
+                        },
+                        "whenTheProductIsUsed": {
+                            "type": "string",
+                            "description": "Time of day or week of use. Eg: early in the morning"
+                        },
+                        "appraisal": {
+                            "type": "string",
+                            "description": "observations on price or value"
+                        },
+                        "quality": {
+                            "type": "string",
+                            "description": "Observations on the quality. Eg: poor quality, great quality"
+                        },
+                        "durability": {
+                            "type": "string",
+                            "description": "Observations on the durability. Eg: not durable, durable, very durable"
+                        },
+                        "easeOfUse": {
+                            "type": "string",
+                            "description": "Observations on the ease of use. Eg: not easy to use, easy to use"
+                        },
+                        "setupAndInstructions": {
+                            "type": "string",
+                            "description": "Observations on the setup. Eg: not easy to set up, easy to set up, easy to follow instructions, not clear instructions"
+                        },
+                        "noiseAndSmell": {
+                            "type": "string",
+                            "description": "Observations on the noise level or smell. Eg: too loud, quiet, squeaky, smells like roses, plastic smell"
+                        },
+                        "sizeAndFit": {
+                            "type": "string",
+                            "description": "Observations on the fit. Eg: too tight, too loose, fits well, too small, too big"
+                        },
+                        "dangerAppraisal": {
+                            "type": "string",
+                            "description": "Observations on the safety of the product. Eg: dangerous, hazardous, safe, can break and harm, safe for children"
+                        },
+                        "designAndAppearance": {
+                            "type": "string",
+                            "description": "Observations on the design and appearance. Eg: not attractive, attractive, love the design, love the appearance"
+                        },
+                        "partsAndComponents": {
+                            "type": "string",
+                            "description": "Observations on the parts and components. Eg: missing parts, all parts included, parts are easy to assemble"
+                        },
+                        "issues": {
+                            "type": "string",
+                            "description": "If specified. Actionable observations on product problems to be addressed. Thorough detailing [max 100 words]. Eg: the product started to rust after one year, although I was expecting it to last 5 years before rusting."
+                        },
                     },
-                    "buyerMotivation": {
-                        "type": "string",
-                        "description": "Reasons why the buyer purchased the product. Eg: to replace an old product, to try out a new product, to give as a gift"
-                    },
-                    "customerExpectations": {
-                        "type": "string",
-                        "description": "Expectations the customer had before purchasing the product. Eg: to be able to use the product for a long time, to be able to use the product in a variety of situations, to be able to use the product for a specific purpose"
-                    },
-                    "howTheProductIsUsed": {
-                        "type": "string",
-                        "description": "Information about what the product is used for or about how the product is used. Eg: doodling, practicing letters/shapes, playing games"
-                    },
-                    "whereTheProductIsUsed": {
-                        "type": "string",
-                        "description": "Suggested locations or situations where the product can be used. Eg: car, restaurant, garden, public parks"
-                    },
-                    "userDescription": {
-                        "type": "string",
-                        "description": "Description of the user for the product. Eg: children, preschoolers, basketball players, mothers, office workers"
-                    },
-                    "packaging": {
-                        "type": "string",
-                        "description": "Description of the product's packaging. Eg: sturdy recyclable box, wrapped in plastic, great for gifting"
-                    },
-                    "season": {
-                        "type": "string",
-                        "description": "Eg: fall and winter"
-                    },
-                    "whenTheProductIsUsed": {
-                        "type": "string",
-                        "description": "Time of day or week of use. Eg: early in the morning"
-                    },
-                    "appraisal": {
-                        "type": "string",
-                        "description": "observations on price or value"
-                    },
-                    "quality": {
-                        "type": "string",
-                        "description": "Observations on the quality. Eg: poor quality, great quality"
-                    },
-                    "durability": {
-                        "type": "string",
-                        "description": "Observations on the durability. Eg: not durable, durable, very durable"
-                    },
-                    "easeOfUse": {
-                        "type": "string",
-                        "description": "Observations on the ease of use. Eg: not easy to use, easy to use"
-                    },
-                    "setupAndInstructions": {
-                        "type": "string",
-                        "description": "Observations on the setup. Eg: not easy to set up, easy to set up, easy to follow instructions, not clear instructions"
-                    },
-                    "noiseAndSmell": {
-                        "type": "string",
-                        "description": "Observations on the noise level or smell. Eg: too loud, quiet, squeaky, smells like roses, plastic smell"
-                    },
-                    "sizeAndFit": {
-                        "type": "string",
-                        "description": "Observations on the fit. Eg: too tight, too loose, fits well, too small, too big"
-                    },
-                    "dangerAppraisal": {
-                        "type": "string",
-                        "description": "Observations on the safety of the product. Eg: dangerous, hazardous, safe, can break and harm, safe for children"
-                    },
-                    "designAndAppearance": {
-                        "type": "string",
-                        "description": "Observations on the design and appearance. Eg: not attractive, attractive, love the design, love the appearance"
-                    },
-                    "partsAndComponents": {
-                        "type": "string",
-                        "description": "Observations on the parts and components. Eg: missing parts, all parts included, parts are easy to assemble"
-                    },
-                    "issues": {
-                        "type": "string",
-                        "description": "If specified. Actionable observations on product problems to be addressed. Thorough detailing [max 100 words]. Eg: the product started to rust after one year, although I was expecting it to last 5 years before rusting."
-                    },
+                    "required": ["reviewSummary", "buyerMotivation", "customerExpectations", "howTheProductIsUsed", "whereTheProductIsUsed", "userDescription", "packaging", "season", "whenTheProductIsUsed", "price", "quality", "durability", "easeOfUse", "setupAndInstructions", "noiseAndSmell", "colors", "sizeAndFit", "dangerAppraisal", "designAndAppearance", "partsAndComponents", "issues"]
                 },
-                "required": ["reviewSummary", "buyerMotivation", "customerExpectations", "howTheProductIsUsed", "whereTheProductIsUsed", "userDescription", "packaging", "season", "whenTheProductIsUsed", "price", "quality", "durability", "easeOfUse", "setupAndInstructions", "noiseAndSmell", "colors", "sizeAndFit", "dangerAppraisal", "designAndAppearance", "partsAndComponents", "issues"]
-            },
-        }
-    ]
-
-    functions = reviewFunctions
-    function_call = {"name": "reviewDataFunction"}
-
-    contentList = []
-    for reviewDict in reviewsList:
-        review = reviewDict['review']
-        messages = [
-            {"role": "user", "content": f"REVIEW: ```{review}```"},
+            }
         ]
-        contentList.append(messages)
 
-    async def main():
-        responses = await get_completion_list(contentList, functions, function_call)
-        return responses
 
-    responses = asyncio.run(main())
+        functions = reviewFunctions
+        function_call = {"name": "reviewDataFunction"}
 
-    evalResponses = []
-    for i, item in enumerate(responses):
-        data = item['function_call']['arguments']
-        data = data.replace('null', 'None')
-        evalData = eval(data)
+        contentList = []
+        for reviewDict in reviewsList:
+            review = reviewDict['review']
+            messages = [
+                {"role": "user", "content": f"REVIEW: ```{review}```"},
+            ]
+            contentList.append(messages)
 
-        evalResponses.append(evalData)
-        reviewsList[i]['insights'] = evalData
+        async def main():
+            responses = await get_completion_list(contentList, functions, function_call)
+            return responses
 
-    newCols = list(reviewsList[1]['insights'].keys())
+        responses = asyncio.run(main())
 
-    for reviewDict in reviewsList:
-        for col in newCols:
-            try:
-                reviewDict[col] = reviewDict['insights'][col]
-            except:
-                reviewDict[col] = None
-        reviewDict.pop('insights')
+        evalResponses = []
+        for i, item in tqdm(enumerate(responses), total=len(responses), desc="Processing reviews"):
+            data = item['function_call']['arguments']
+            data = data.replace('null', 'None')
+            evalData = eval(data)
 
-    write_reviews_to_firestore(reviewsList, db)
-    return reviewsList
+            evalResponses.append(evalData)
+            reviewsList[i]['insights'] = evalData
 
+        newCols = list(reviewsList[1]['insights'].keys())
+
+        for reviewDict in tqdm(reviewsList, desc="Updating review dictionaries"):
+            for col in newCols:
+                try:
+                    reviewDict[col] = reviewDict['insights'][col]
+                except KeyError as e:
+                    logging.warning(f"KeyError: {e}")
+                    reviewDict[col] = None
+            reviewDict.pop('insights')
+
+        write_reviews_to_firestore(reviewsList, db)
+
+        # Stop the timer and print the elapsed time
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Total time taken for processing: {elapsed_time:.2f} seconds")
+
+        return reviewsList
+
+    except Exception as e:
+        logging.error(f"Error in process_reviews_with_gpt: {e}")
+        return None  # Return None in case of error
 
 
 ############################################ RUN ############################################
