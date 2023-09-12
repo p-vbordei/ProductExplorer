800d586080ec01c530b46ee8c224da685f624e42
Vlad <bordeivlad@gmail.com>
2023-09-12 11:55:32 +0300

    before transforming to a single function

--- src/batch_reviews_processing.py
+++ src/batch_reviews_processing.py
@@ -7,113 +7,22 @@ import time
 import logging
 import pandas as pd
 logging.basicConfig(level=logging.INFO)
+import tiktoken
+import json
 
 try:
     from src import app
-    from src.reviews_data_processing_utils import process_datapoints, quantify_observations
+    from src.reviews_data_processing_utils import process_datapoints, quantify_observations, generate_batches, add_uid_to_reviews, aggregate_all_categories, extractTagsForReview
     from src.firebase_utils import initialize_firestore, get_clean_reviews , write_reviews_to_firestore, save_cluster_info_to_firestore, write_insights_to_firestore
     from src.openai_utils import get_completion_list_multifunction
     from src.reviews_clustering import cluster_reviews, label_clusters
     from src.investigations import update_investigation_status
 except ImportError:
-    from reviews_data_processing_utils import process_datapoints, quantify_observations
+    from reviews_data_processing_utils import process_datapoints, quantify_observations, generate_batches, add_uid_to_reviews, aggregate_all_categories, extractTagsForReview
     from firebase_utils import initialize_firestore, get_clean_reviews , write_reviews_to_firestore, save_cluster_info_to_firestore, write_insights_to_firestore
     from openai_utils import get_completion_list_multifunction
     from reviews_clustering import cluster_reviews, label_clusters
     from investigations import update_investigation_status
-
-# %%
-
-import tiktoken
-
-def transform_rating_to_star_format(rating):
-    """
-    Transforms a numerical or textual rating to a star format.
-
-    Args:
-    - rating (int or str): A numerical rating value between 1 to 5 or a textual rating.
-
-    Returns:
-    - str: A string representation of the rating in the format "5*" or "1*", or the textual rating followed by a "*".
-    """
-    
-    if isinstance(rating, int) and 1 <= rating <= 5:
-        return f"{rating}*"
-    elif isinstance(rating, str):
-        return f"{rating}*"
-    else:
-        raise ValueError("Rating must be a number between 1 and 5 or a textual value.")
-
-def add_uid_to_reviews(reviewsList):
-    """
-    Adds a 'uid' to each review in the reviewsList based on its index.
-
-    Args:
-    - reviewsList (list): List of dictionaries with reviews.
-
-    Returns:
-    - list: Updated list of reviews with 'uid' added.
-    """
-
-    # Extract 'id' from each dictionary
-    ids = [review['id'] for review in reviewsList]
-
-    # Create DataFrame
-    id_uid_df = pd.DataFrame(ids, columns=['id'])
-    id_uid_df['uid'] = id_uid_df.index
-
-    # Create a mapping of 'id' to 'uid'
-    id_to_uid_mapping = id_uid_df.set_index('id')['uid'].to_dict()
-
-    # Add 'uid' to each dictionary in reviewsList
-    for review in reviewsList:
-        review['uid'] = id_to_uid_mapping.get(review['id'], None)
-
-    return reviewsList
-
-def num_tokens_from_string(string: str, encoding_name = "cl100k_base") -> int:
-    """Returns the number of tokens in a text string."""
-    encoding = tiktoken.get_encoding(encoding_name)
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
-
-
-# SPLIT TO BATCHES OF 'x' tokens
-def generate_batches(reviews, max_tokens=6000):
-    """
-    This function takes a list of reviews and groups them into batches based on token count. Each batch 
-    has a token count that doesn't exceed the specified max_tokens limit. It returns a list of batches, 
-    where each batch is a list of tuples. Each tuple contains a review ID and its corresponding text.
-    
-    Args:
-        reviews (list of dicts): A list of review dictionaries. Each dictionary has keys 'id' and 'text'.
-        max_tokens (int): The maximum number of tokens allowed per batch.
-
-    Returns:
-        batches (list): A list of lists, where each inner list represents a batch of tuples.
-    """
-    batches = []
-    current_batch = []
-    current_tokens = 0
-
-    for review in reviews:
-        review_id = review['uid']
-        review_text = review['text']
-        review_rating = transform_rating_to_star_format(review['rating'])
-
-        imp_tokens = num_tokens_from_string(review_text, encoding_name="cl100k_base")
-        if current_tokens + imp_tokens + 1 <= max_tokens:
-            current_batch.append((review_id,review_rating, review_text))
-            current_tokens += imp_tokens + 1
-        else:
-            batches.append(current_batch)
-            current_batch = [(review_id, review_rating, review_text)]
-            current_tokens = imp_tokens
-    if current_batch:
-        batches.append(current_batch)
-
-    return batches
-
 
 # %%
 
@@ -135,14 +44,30 @@ except Exception as e:
     logging.error(f"Error getting clean reviews: {e}")
 
 # %%
-updated_reviewsList = add_uid_to_reviews(reviewsList)
+# Allocate short Ids to reviews
+updatedReviewsList = add_uid_to_reviews(reviewsList)
 
-
-# %%
 # Prepare Review Batches
-review_batches = generate_batches(updated_reviewsList, max_tokens=8000)
+reviewBatches = generate_batches(updatedReviewsList, max_tokens=1000)
+
 
 # %%
+# Generate Content List for Batches
+contentList = []
+
+for batch in reviewBatches:
+    batch_review = f"\n\n <Review uIds>  will be followed by <Review Rating> and than by  `review text`:"
+    batch_review += "\n\n".join([f"<{review_id}>\n,<{review_rating}>\n,`{review_text}`" for review_id, review_rating, review_text in batch])
+    
+    messages = [
+        {"role": "user", "content": batch_review},
+    ]
+    contentList.append(messages)
+
+logging.info("Content list prepared.")
+# %%
+
+# Define functions for data extraction
 marketUserAnalysisReviewFunctions = [
     {
         "name": "marketUserAnalysis",
@@ -159,7 +84,7 @@ marketUserAnalysisReviewFunctions = [
                             "label": {
                                 "type": "string",
                             },
-                            "id": {
+                            "uid": {
                                 "type": "array",
                                 "items": {
                                     "type": "number"
@@ -177,7 +102,7 @@ marketUserAnalysisReviewFunctions = [
                             "label": {
                                 "type": "string",
                             },
-                            "id": {
+                            "uid": {
                                 "type": "array",
                                 "items": {
                                     "type": "number"
@@ -195,7 +120,7 @@ marketUserAnalysisReviewFunctions = [
                             "label": {
                                 "type": "string",
                             },
-                            "id": {
+                            "uid": {
                                 "type": "array",
                                 "items": {
                                     "type": "number"
@@ -213,7 +138,7 @@ marketUserAnalysisReviewFunctions = [
                             "label": {
                                 "type": "string",
                             },
-                            "id": {
+                            "uid": {
                                 "type": "array",
                                 "items": {
                                     "type": "number"
@@ -231,7 +156,7 @@ marketUserAnalysisReviewFunctions = [
                             "label": {
                                 "type": "string",
                             },
-                            "id": {
+                            "uid": {
                                 "type": "array",
                                 "items": {
                                     "type": "number"
@@ -261,7 +186,7 @@ functionalEmotionalAnalysisReviewFunctions = [
                             "label": {
                                 "type": "string",
                             },
-                            "id": {
+                            "uid": {
                                 "type": "array",
                                 "items": {
                                     "type": "number"
@@ -279,7 +204,7 @@ functionalEmotionalAnalysisReviewFunctions = [
                             "label": {
                                 "type": "string",
                             },
-                            "id": {
+                            "uid": {
                                 "type": "array",
                                 "items": {
                                     "type": "number"
@@ -297,7 +222,10 @@ functionalEmotionalAnalysisReviewFunctions = [
                             "label": {
                                 "type": "string",
                             },
-                            "id": {
+                            "description": {
+                                "type": "string",
+                            },
+                            "uid": {
                                 "type": "array",
                                 "items": {
                                     "type": "number"
@@ -315,7 +243,7 @@ functionalEmotionalAnalysisReviewFunctions = [
                             "label": {
                                 "type": "string",
                             },
-                            "id": {
+                            "uid": {
                                 "type": "array",
                                 "items": {
                                     "type": "number"
@@ -333,7 +261,7 @@ functionalEmotionalAnalysisReviewFunctions = [
                             "label": {
                                 "type": "string",
                             },
-                            "id": {
+                            "uid": {
                                 "type": "array",
                                 "items": {
                                     "type": "number"
@@ -349,91 +277,68 @@ functionalEmotionalAnalysisReviewFunctions = [
 
 
 # %%
-
-#functions = marketUserAnalysisReviewFunctions
-#function_call = {"name": "marketUserAnalysis"}
-
-
-# %%
-#functions =  functionalEmotionalAnalysisReviewFunctions
-#function_call = {"name": "functionalEmotionalAnalysis"}
-
-# %%
-# Generate Content List for Batches
-contentList = []
-
-for batch in review_batches:
-    batch_review = f"\n\n <Review IDs>  will be followed by <Review Rating> and than by  `review text`:"
-    batch_review += "\n\n".join([f"<{review_id}>\n,<{review_rating}>\n,`{review_text}`" for review_id, review_rating, review_text in batch])
-    
-    messages = [
-        {"role": "user", "content": batch_review},
-    ]
-    contentList.append(messages)
-
-logging.info("Content list prepared.")
-
-
-# %%
 GPT_MODEL = 'gpt-3.5-turbo-16k'
 
 async def main():
     # List of function sets
-    functions_list = [
+    functionsList = [
         marketUserAnalysisReviewFunctions,
         functionalEmotionalAnalysisReviewFunctions
     ]
     
     # List of function calls
-    function_calls_list = [
+    functionsCallList = [
         {"name": "marketUserAnalysis"},
         {"name": "functionalEmotionalAnalysis"}
     ]
     
-    responses = await get_completion_list_multifunction(contentList, functions_list, function_calls_list, GPT_MODEL=GPT_MODEL)
+    responses = await get_completion_list_multifunction(contentList, functionsList, functionsCallList, GPT_MODEL=GPT_MODEL)
     return responses
 
 responses = asyncio.run(main())
 
-# %%
 
+# %%
 evalResponses = []
 
-for batch, item in zip(review_batches, responses):
-    try:
-        # Extracting the data from the response item
-        data = item['function_call']['arguments']
-        data = data.replace('null', 'None')
-        evalData = eval(data)
-        evalResponses.append(evalData)
-    except Exception as e:
-        logging.error(f"Error processing response item: {e}")
+functionsCallList = [
+        {"name": "marketUserAnalysis"},
+        {"name": "functionalEmotionalAnalysis"} ]
 
-# Function to extract tags for a review
-def extractTagsForReview(reviewUid, evalData):
-    tags = {}
-    try:
-        for topic, labels in evalData.items():
-            for label in labels:
-                if reviewUid in label['id']:
-                    tags[topic] = tags.get(topic, [])
-                    tags[topic].append(label['label'])
-    except Exception as e:
-        logging.error(f"Error extracting tags for review with UID {reviewUid}: {e}")
-    return tags
+response_index = 0
+try:
+    for batch in reviewBatches:
+        for function in functionsCallList:
+            item = responses[response_index]
+            # Extracting the data from the response item
+            data = item['function_call']['arguments']
+            evalData = json.loads(data)
+            evalResponses.append(evalData)
+            response_index += 1
+except Exception as e:
+    logging.error(f"Error processing response item at index {response_index}: {e}")
+    logging.error(f"Data causing the error: {data}")
+
+# %%
+try:
+    aggregatedResponses = aggregate_all_categories(evalResponses)
+except Exception as e:
+    logging.error(f"Error in aggregating responses: {e}")
+    aggregatedResponses = {}
+
+# %%
 
 # Updating the reviewsList to include tags
-for reviewDict in reviewsList:
-    try:
+try:
+    for reviewDict in updatedReviewsList:
         reviewUid = reviewDict['uid']
         tagsForReview = {}
-        for evalData in evalResponses:
+        for evalData in aggregatedResponses:
             tags = extractTagsForReview(reviewUid, evalData)
             tagsForReview.update(tags)
         if tagsForReview:
             reviewDict['tags'] = tagsForReview
-    except Exception as e:
-        logging.error(f"Error processing review with UID {reviewUid}: {e}")
-
+except Exception as e:
+    logging.error(f"Error updating review list with tags for UID {reviewUid}: {e}")
 
 # %%
--- src/reviews_data_processing_utils.py
+++ src/reviews_data_processing_utils.py
@@ -7,6 +7,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 import numpy as np
 import pandas as pd
+import tiktoken
 
 def num_tokens_from_string(string: str, encoding_name="cl100k_base") -> int:
     """Returns the number of tokens in a text string."""
@@ -134,3 +135,151 @@ def quantify_observations(reviewsWithClusters, cleanReviews):
 
 
 # =====================
+
+
+
+
+def transform_rating_to_star_format(rating):
+    """
+    Transforms a numerical or textual rating to a star format.
+
+    Args:
+    - rating (int or str): A numerical rating value between 1 to 5 or a textual rating.
+
+    Returns:
+    - str: A string representation of the rating in the format "5*" or "1*", or the textual rating followed by a "*".
+    """
+    
+    if isinstance(rating, int) and 1 <= rating <= 5:
+        return f"{rating}*"
+    elif isinstance(rating, str):
+        return f"{rating}*"
+    else:
+        raise ValueError("Rating must be a number between 1 and 5 or a textual value.")
+
+def add_uid_to_reviews(reviewsList):
+    """
+    Adds a 'uid' to each review in the reviewsList based on its index.
+
+    Args:
+    - reviewsList (list): List of dictionaries with reviews.
+
+    Returns:
+    - list: Updated list of reviews with 'uid' added.
+    """
+
+    # Extract 'id' from each dictionary
+    ids = [review['id'] for review in reviewsList]
+
+    # Create DataFrame
+    id_uid_df = pd.DataFrame(ids, columns=['id'])
+    id_uid_df['uid'] = id_uid_df.index
+
+    # Create a mapping of 'id' to 'uid'
+    id_to_uid_mapping = id_uid_df.set_index('id')['uid'].to_dict()
+
+    # Add 'uid' to each dictionary in reviewsList
+    for review in reviewsList:
+        review['uid'] = id_to_uid_mapping.get(review['id'], None)
+
+    return reviewsList
+
+
+
+# SPLIT TO BATCHES OF 'x' tokens
+def generate_batches(reviews, max_tokens):
+    """
+    This function takes a list of reviews and groups them into batches based on token count. Each batch 
+    has a token count that doesn't exceed the specified max_tokens limit. It returns a list of batches, 
+    where each batch is a list of tuples. Each tuple contains a review ID and its corresponding text.
+    
+    Args:
+        reviews (list of dicts): A list of review dictionaries. Each dictionary has keys 'id' and 'text'.
+        max_tokens (int): The maximum number of tokens allowed per batch.
+
+    Returns:
+        batches (list): A list of lists, where each inner list represents a batch of tuples.
+    """
+    batches = []
+    current_batch = []
+    current_tokens = 0
+
+    for review in reviews:
+        review_id = review['uid']
+        review_text = review['text']
+        review_rating = transform_rating_to_star_format(review['rating'])
+
+        imp_tokens = num_tokens_from_string(review_text, encoding_name="cl100k_base")
+        if current_tokens + imp_tokens + 1 <= max_tokens:
+            current_batch.append((review_id,review_rating, review_text))
+            current_tokens += imp_tokens + 1
+        else:
+            batches.append(current_batch)
+            current_batch = [(review_id, review_rating, review_text)]
+            current_tokens = imp_tokens
+    if current_batch:
+        batches.append(current_batch)
+
+    return batches
+
+
+
+
+def aggregate_all_categories(data):
+    """
+    Aggregate items from all products under the same broad groupings.
+
+    Parameters:
+    - data (list): List of dictionaries representing products/categories.
+
+    Returns:
+    - dict: Aggregated items under the broad groupings.
+    """
+    aggregated_results = {}
+    try:
+        for product in data:
+            for category_name, items in product.items():
+                if category_name not in aggregated_results:
+                    aggregated_results[category_name] = []
+                aggregated_results[category_name].extend(items)
+    except Exception as e:
+        logging.error(f"Error in aggregate_all_categories: {e}")
+        return {}
+    return aggregated_results
+
+
+
+def extractTagsForReview(reviewUid, evalData):
+    """
+    Extract tags associated with a specific review UID from the evaluation data.
+
+    Parameters:
+    - reviewUid (int or str): The unique identifier for the review.
+    - evalData (dict): Dictionary containing evaluation data with topics and labels.
+
+    Returns:
+    - dict: A dictionary containing topics as keys and associated labels as values.
+
+    Notes:
+    - If the evalData is not a dictionary, an error is logged and an empty dictionary is returned.
+    - If there's an error during processing, it's logged and an empty dictionary is returned.
+    """
+    
+    tags = {}
+    try:
+        # Check if evalData is a dictionary
+        if not isinstance(evalData, dict):  
+            logging.error(f"evalData is not a dictionary for review UID {reviewUid}. Data: {evalData}")
+            return tags  # Return empty tags if evalData is not a dictionary
+
+        # Loop through topics and labels in evalData
+        for topic, labels in evalData.items():
+            for label in labels:
+                # Check if the review UID matches with the label's UID
+                if reviewUid in label['uid']:
+                    tags[topic] = tags.get(topic, [])
+                    tags[topic].append(label['label'])
+    except Exception as e:
+        logging.error(f"Error in extractTagsForReview for UID {reviewUid}: {e}")
+        return {}
+    return tags
